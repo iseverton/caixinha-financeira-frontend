d858b438c5dec171aebd0e627765f0e9043d7e64
Everton Ferreira <iseverton2024@gmail.com>
2025-11-25 08:58:27 -0300

    feat: implement goal service

---
+++ src/app/core/goal-service.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { goal } from '../shared/components/models/goal';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GoalService {
+  private baseurl: string = 'https://localhost:7283/api/goals/';
+  private http = inject(HttpClient);
+
+  CreateGoal(body: goal): Observable<goal> {
+    return this.http.post<goal>(this.baseurl, body);
+  }
+
+  listAllGoals(): Observable<goal[]> {
+    return this.http.get<goal[]>(this.baseurl);
+  }
+
+  Deposit(id: any, amount: number) {
+    return this.http.post<goal>(this.baseurl + id + '/deposit', amount);
+  }
+
+  totalSaved() {
+    return this.http.get<any>(this.baseurl + 'total');
+  }
+
+  totalGoals() {
+    return this.http.get<any>(this.baseurl + 'total-goals');
+  }
+
+  GetActiveGoals() {
+    return this.http.get<any>(this.baseurl + 'active-goals');
+  }
+}
